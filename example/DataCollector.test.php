98ad2c490f0669ffa824b5867c8881073d3cfb76
Alfred Tchondjo <iriven@yahoo.fr>
2017-04-09 10:12:53 +0200

    Update DataCollector.test.php

--- example/DataCollector.test.php
+++ example/DataCollector.test.php
@@ -1,1 +1,36 @@
 <?php
+//Usage:
+
+$Author =  new \Iriven\IrivenPHPDataCollector([
+                            'name'          => $name,
+                            'email'         => $email,
+                            'homepage'      => $homepage,
+                            'role'          => $role,
+                            'description'   => $description
+                        ]);
+                        
+$Author->get('name',null);
+$Author->get('email',null);
+$Author->get('homepage',null);
+$Author->get('role',null);
+$Author->get('description',null);
+
+// Other methodes:
+
+//** methode 1:
+- $Author =  new \Iriven\IrivenPHPDataCollector();
+- $Author->add([
+                            'name'          => $name,
+                            'email'         => $email,
+                            'homepage'      => $homepage,
+                            'role'          => $role,
+                            'description'   => $description
+                        ]);
+
+//** methode 2:
+$Author =  new \Iriven\IrivenPHPDataCollector();
+$Author->set('name',$name);
+$Author->set('email', $email);
+$Author->set('homepage',$homepage);
+$Author->set('role', $role);
+$Author->set('description',$description);
